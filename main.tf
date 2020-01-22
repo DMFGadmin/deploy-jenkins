3cdddfb618c2c4bf9b0a9fdf68a49f272f390dc1
joel <joelcote68@gmail.com>
2020-01-22 12:40:45 -0600

    added 2nd IP block for access

--- main.tf
+++ main.tf
@@ -3,7 +3,8 @@ module "deploy_jenkins" {
   network = var.network
   project_id = var.project_id
   jenkins_access_source_tags = var.jenkins_access_source_tags
-  source_ranges = var.source_ranges
+  source_range1 = var.source_range1
+  source_range2 = var.source_range2
   subnetwork = var.subnetwork
   zone      = var.zone
 }
