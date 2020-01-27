0b10306593cb11819798b9ce5614688f719a487e
joel <joelcote68@gmail.com>
2020-01-27 13:37:56 -0600

    outputs testing

--- main.tf
+++ main.tf
@@ -1,10 +1,7 @@
 module "deploy_jenkins" {
   source          = "./modules/jenkins"
-  network = var.network
-  project_id = var.project_id
   jenkins_access_source_tags = var.jenkins_access_source_tags
   source_range1 = var.source_range1
   source_range2 = var.source_range2
-  subnetwork = var.subnetwork
   zone      = var.zone
 }
